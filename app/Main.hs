386d6dfe50ec3fc4b13493eb10cb54453ff42a95
ernesto alvarado <ernesto.alvarado@adroll.com>
2019-04-21 14:13:20 -0500

    creado crea poli

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,17 @@ monomio coeficiente exponente x = (fromIntegral coeficiente)
                                  * ((fromIntegral x)
                                     ^^ (fromIntegral exponente))
 
--- polinomio::[(Int, Int)]->(Int->Int->Int->Double)->[(Int->Double)]
--- polinomio [] _ p = p
--- polinomio (coef, exp):xs f p = polinomio xs f p:(f coef exp)
-
 polinomio::[(Int, Int)]->(Int->Int->Int->Double)->[(Int->Double)]->[(Int->Double)]
 polinomio [] _ p = p
 polinomio ((coef,expo):xs) f p = polinomio xs f (p ++ [f coef expo])
+
+evalua_polimonio::[(Int->Double)]->Int->Double
+evalua_polimonio p x = foldl (\acc m -> acc + (m x)) 0.0 p
+
+crea_polinomio::[Int]->[Int]->[(Int->Double)]->[(Int->Double)]
+crea_polinomio [] _ p = p
+crea_polinomio _ [] p = p
+crea_polinomio a b p = polinomio (zip a b) monomio []
 
 main :: IO ()
 main = printf "%f\n" (monomio (-10) (-5) (31))
