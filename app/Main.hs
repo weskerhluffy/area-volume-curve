744d49f44d5b491dff89aeb084e3f0dec9773194
ernesto alvarado <ernesto.alvarado@adroll.com>
2019-04-22 20:58:36 -0500

    funcion area creada

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,26 @@
 import Text.Printf
-monomio::Int->Int->Int->Double
+monomio::Int->Int->Double->Double
 monomio coeficiente exponente x = (fromIntegral coeficiente)
-                                 * ((fromIntegral x)
+                                 * (x
                                     ^^ (fromIntegral exponente))
 
-polinomio::[(Int, Int)]->(Int->Int->Int->Double)->[(Int->Double)]->[(Int->Double)]
+polinomio::[(Int, Int)]->(Int->Int->Double->Double)->[(Double->Double)]->[(Double->Double)]
 polinomio [] _ p = p
 polinomio ((coef,expo):xs) f p = polinomio xs f (p ++ [f coef expo])
 
-evalua_polimonio::[(Int->Double)]->Int->Double
+evalua_polimonio::[(Double->Double)]->Double->Double
 evalua_polimonio p x = foldl (\acc m -> acc + (m x)) 0.0 p
 
-crea_polinomio::[Int]->[Int]->[(Int->Double)]->[(Int->Double)]
-crea_polinomio [] _ p = p
-crea_polinomio _ [] p = p
-crea_polinomio a b p = polinomio (zip a b) monomio []
+crea_polinomio::[Int]->[Int]->[(Double->Double)]
+crea_polinomio a b = polinomio (zip a b) monomio []
+
+area::[(Double->Double)]->Int->Int->Double
+area p l r = sum $ map (evalua_polimonio p) $ takeWhile (<(fromIntegral r)) [(fromIntegral l)+0.05::Double,(fromIntegral l)+(0.1::Double)..]
+
+calcula_area::[Int]->[Int]->Int->Int->Double
+calcula_area coeficientes exponentes l r = area p l r
+  where p = crea_polinomio coeficientes exponentes
 
 main :: IO ()
+-- main = printf "%f\n" (monomio (-10) (-5) (31))
 main = printf "%f\n" (monomio (-10) (-5) (31))
